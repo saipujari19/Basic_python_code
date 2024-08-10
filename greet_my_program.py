13088ba75b0f7ba0278cb0509dea734ea1675b0c
saipujari19 <168142000+saipujari19@users.noreply.github.com>
2024-08-10 17:14:53 -0400

    Add files via upload

---
+++ greet_my_program.py
@@ -0,0 +1,5 @@
+#creating a greeting for my program.
+print("welcome to the brand name generator!!")
+user_city = input("The city where you grew up? \n")
+user_pet = input("Which is your favourite pet? \n")
+print("your brand name could be:" + user_city +" " + user_pet)
