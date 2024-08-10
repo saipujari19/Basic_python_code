d318d6b53a14bd65ef8db6a778bef2e9a0d717df
saipujari19 <168142000+saipujari19@users.noreply.github.com>
2024-08-10 17:10:55 -0400

    Add files via upload

---
+++ sumne.py
@@ -0,0 +1,21 @@
+#
+# #print("hi " + input("what's your name: ") + "!") #takes input from the user and
+# #then prints hi message along with the name also concatinated with the string to add extra at the end.
+# # input("what's your name: ") input(), is a function
+# #print(input("hi") + input("whats your name:"))
+# variables are nothing but the names given to the action. name = "sai" print(name),  also it can be modified.
+# name = input("enter the name: ")
+# print(len(name))
+#print(len(input("enter the name:")))#printing the output in single line.
+# username = input("enter the name: ")
+# length = len(username)# this can be assigned to a variable and then print it.
+# print(length)
+
+# creating a greeting for my program.
+# print("welcome to the brand name generator!!")
+# user_city = input("The city where you grew up? \n")
+# user_pet = input("Which is your favourite pet? \n")
+# print("your brand name could be:" + user_city +" " + user_pet)
+
+print("king of india" [5])#SUBSCRIPTING AND WE CAN GET THE INDIVIDUAL CHARACTER FROM THE STRING BY INDEXING.
+# even we can give negative indexing, but it starts counting from the backwards
